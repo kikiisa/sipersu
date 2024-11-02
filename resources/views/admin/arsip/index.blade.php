c21235ef58fbf9d3c1e7a0b34b81117a2bd33726
kikiisa <kikiisa89@gmail.com>
2024-11-02 08:50:53 +0700

    feat:icon

--- resources/views/admin/arsip/index.blade.php
+++ resources/views/admin/arsip/index.blade.php
@@ -146,7 +146,7 @@
                         </li>
                         @for ($i = 1; $i <= $data->lastPage(); $i++)
                             <li>
-                                <a href="{{ $data->url($i) }}" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white {{ $data->currentPage() == $i ? 'bg-slate-500 text-white' : '' }}">{{ $i }}</a>
+                                <a href="{{ $data->url($i) }}" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white {{ $data->currentPage() == $i ? 'bg-blue-500 text-white' : '' }}">{{ $i }}</a>
                             </li>
                         @endfor
                         <li>
